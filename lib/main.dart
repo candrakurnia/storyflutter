ad181e77d462bab6b59efae3319a66c5fa4d61af
Candra Kurnia <candrakurnianugraha@gmail.com>
2024-09-19 10:33:55 +0700

    feat : add localizations

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:storyflutter/api/api_service.dart';
+import 'package:storyflutter/common/common.dart';
 import 'package:storyflutter/db/auth_repository.dart';
 import 'package:storyflutter/provider/all_stories_provider.dart';
 import 'package:storyflutter/provider/auth_provider.dart';
 import 'package:storyflutter/provider/detail_story_provider.dart';
+import 'package:storyflutter/provider/localization_provider.dart';
 import 'package:storyflutter/provider/login_provider.dart';
 import 'package:storyflutter/provider/register_provider.dart';
 import 'package:storyflutter/provider/upload_provider.dart';
@@ -23,7 +25,6 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  
   late AuthProvider authProvider;
   late MyRouteInformationParser myRouterInformationParser;
   late MyRouterDelegate myRouterDelegate;
@@ -38,26 +39,40 @@ class _MyAppState extends State<MyApp> {
 
     myRouterDelegate = MyRouterDelegate(authRepository);
     myRouterInformationParser = MyRouteInformationParser();
-
   }
+
   @override
   Widget build(BuildContext context) {
     return MultiProvider(
       providers: [
         ChangeNotifierProvider(create: (context) => authProvider),
-        ChangeNotifierProvider(create: (context) => LoginProvider(apiService: ApiService())),
-        ChangeNotifierProvider(create: (context) => RegisterProvider(apiService: ApiService())),
-        ChangeNotifierProvider(create: (context) => AllStoriesProvider(apiService: ApiService())),
-        ChangeNotifierProvider(create: (context) => DetailStoryProvider(apiService: ApiService())),
-        ChangeNotifierProvider(create: (context) => UploadProvider(apiService: ApiService())),
+        ChangeNotifierProvider(
+            create: (context) => LoginProvider(apiService: ApiService())),
+        ChangeNotifierProvider(
+            create: (context) => RegisterProvider(apiService: ApiService())),
+        ChangeNotifierProvider(
+            create: (context) => AllStoriesProvider(apiService: ApiService())),
+        ChangeNotifierProvider(
+            create: (context) => DetailStoryProvider(apiService: ApiService())),
+        ChangeNotifierProvider(
+            create: (context) => UploadProvider(apiService: ApiService())),
+        ChangeNotifierProvider<LocalizationProvider>(
+          create: (context) => LocalizationProvider(),
+          builder: (context, child) {
+            final provider = Provider.of<LocalizationProvider>(context);
+            return MaterialApp.router(
+              localizationsDelegates: AppLocalizations.localizationsDelegates,
+              supportedLocales: AppLocalizations.supportedLocales,
+              locale: provider.locale,
+              debugShowCheckedModeBanner: false,
+              title: "StoryApp",
+              routerDelegate: myRouterDelegate,
+              routeInformationParser: myRouterInformationParser,
+              backButtonDispatcher: RootBackButtonDispatcher(),
+            );
+          },
+        ),
       ],
-      child: MaterialApp.router(
-        debugShowCheckedModeBanner: false,
-        title: "StoryApp",
-        routerDelegate: myRouterDelegate,
-        routeInformationParser: myRouterInformationParser,
-        backButtonDispatcher: RootBackButtonDispatcher(),
-      ),
     );
   }
 }
